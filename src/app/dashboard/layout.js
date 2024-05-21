8eda51231bdf8abff33c86a43b3983ae7ef3affb
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-05-21 16:58:40 +0330

    adding dashboard and sidebar to dashboard

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,12 @@
+
+import DashboardSidebar from '@/layout/DashboardSidebar';
+
+
+function DashboardLayout({children}) {
+    
+  return (
+    <DashboardSidebar>{children}</DashboardSidebar>
+  )
+}
+
+export default DashboardLayout
---
+++ src/components/layout/DashboardSidebar.js
@@ -0,0 +1,26 @@
+
+import styles from "@/layout/DashboardSidebar.module.css"
+import {CgProfile} from "react-icons/cg";
+import {getServerSession} from "next-auth";
+import{authOptions} from "@/app/api/auth/[...nextauth]/route";
+import Link from "next/link"
+
+
+async function DashboardSidebar({children}) {
+    const session = await getServerSession(authOptions);
+  return (
+    <div className={styles.container}>
+        <div className={styles.sidebar}>
+            <CgProfile />
+            <p>{session.user.email}</p>
+            <span></span>
+            <Link href="/dashboard">حساب کاربری</Link>
+            <Link href="/dashboard/my-profiles">آگهی های من</Link>
+            <Link href="/dashboard/add">ثبت آگهی</Link>
+        </div>
+        <div className={styles.main}>{children}</div>
+    </div>
+  )
+}
+
+export default DashboardSidebar
